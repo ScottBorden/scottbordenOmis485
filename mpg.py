f599122cbc80f392a537d494786ec4586e069baf
ScottBorden <47012912+ScottBorden@users.noreply.github.com>
2019-03-31 23:08:26 -0500

    Add files via upload

--- mpg.py
+++ mpg.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python3
 
 # display a welcome message
-print("The Miles Per Gallon program")
+print("The Miles Per Gallon application")
 print()
 
-# get input from the user
-miles_driven= float(input("Enter miles driven:\t\t"))
-gallons_used = float(input("Enter gallons of gas used:\t"))
-cost_per_gallon = float(input("Enter cost per gallon:\t"))
+choice = "y"
 
-# calculate miles per gallon
-mpg = miles_driven / gallons_used
-mpg = round(mpg, 2)
+while choice == "y":
+    # get input from the user
+    miles_driven = float(input("Enter miles driven:         "))
+    gallons_used = float(input("Enter gallons of gas used:  "))
+    cost_per_gallon = float(input("Enter cost per gallon:\t\t"))
 
-gas_cost = gallons_used+cost_per_gallon
-gas_cost = round(gas_cost,2)
+    if miles_driven <= 0:
+        print("Miles driven must be greater than zero. Please try again.")
+    elif gallons_used <= 0:
+        print("Gallons used must be greater than zero. Please try again.")
+    else:
+    # calculate and display miles per gallon
+        mpg = round((miles_driven / gallons_used), 2)
+        gas_cost = round((gallons_used * cost_per_gallon),2)
+        cost_per_mile = round((gas_cost / miles_driven),1)
+        
+        print()
+        print("Miles Per Gallon:          ", mpg)
+        print("Total Gas Cost:            ", gas_cost)
+        print("Cost Per Mile:             ", cost_per_mile)
 
-cost_per_mile = gas_cost/miles_driven
-cost_per_mile = round(cost_per_mile,2)         
-# format and display the result
-print()
-print("Miles Per Gallon:\t\t" + str(mpg))
-print("Total Gas Cost:\t\t" + str(gas_cost))
-print("Cost per mile:\t" + str(cost_per_mile))
-print("Bye")
+    print()
+    yes_or_no = (input("Get entries for another trip (y/n)?")
+    if yes_or_no == 'n':
+        break
+
+
 
 
